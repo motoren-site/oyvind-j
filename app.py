7789e8e423fac18a821dc216c1a3cd1d9ac4f3c2
Øyvind Johannessen <oyvind@yvinds-MBP-2.miles.local>
2022-09-15 15:17:45 +0200

    :)

--- app.py
+++ app.py
@@ -2,12 +2,8 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/hello')
-def index():
-    return 'Hello world!'
-
 @app.route('/')
-def home():
+def index():
     return 'Hello world!'
 
 app.run(app.run(host='0.0.0.0', port=5000))
